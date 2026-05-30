1c6a7882658bd70ba0a951be3add75a1cbd155dc
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-05-31 01:35:50 +0300

    Update Sell Lemons [7395930870].lua

--- games/Sell Lemons [7395930870].lua
+++ games/Sell Lemons [7395930870].lua
@@ -8,7 +8,18 @@ end
 
 _G.AutoClick = not _G.AutoClick
 local Click = tycoon.Remotes.WakeIncomeStream
-while _G.AutoClick and not tycoon.Purchases["Lemon Stand"].Other:FindFirstChild("Cash Register") do
-    Click:InvokeServer("LemonStand")
-    task.wait(.9)
-end
+task.spawn(function()
+    while _G.AutoClick and not tycoon.Purchases["Lemon Stand"].Other:FindFirstChild("Cash Register") do
+        Click:InvokeServer("LemonStand")
+        task.wait(.9)
+    end
+end)
+
+_G.AutoUpgrade = not _G.AutoUpgrade
+local Upgrade = tycoon.Purchases["Lemon Stand"]["Lemon Stand"]["Lemon Stand"].Upgrade
+task.spawn(function()
+    while _G.AutoUpgrade do
+        Upgrade:InvokeServer(1)
+        task.wait()
+    end
+end)
