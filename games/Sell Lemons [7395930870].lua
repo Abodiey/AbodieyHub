762ed5c952f4fa9c22ac378483881a1220ea7a36
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-05-31 01:40:25 +0300

    Update Sell Lemons [7395930870].lua

--- games/Sell Lemons [7395930870].lua
+++ games/Sell Lemons [7395930870].lua
@@ -6,20 +6,38 @@ for i,v in pairs(workspace:GetChildren()) do
     tycoon = v
 end
 
-_G.AutoClick = not _G.AutoClick
-local Click = tycoon.Remotes.WakeIncomeStream
+_G.AutoClickLemonStand = not _G.AutoClickLemonStand
+local ClickLemonStand = tycoon.Remotes.WakeIncomeStream
 task.spawn(function()
-    while _G.AutoClick and not tycoon.Purchases["Lemon Stand"].Other:FindFirstChild("Cash Register") do
-        Click:InvokeServer("LemonStand")
+    while _G.AutoClickLemonStand and not tycoon.Purchases["Lemon Stand"].Other:FindFirstChild("Cash Register") do
+        ClickLemonStand:InvokeServer("LemonStand")
         task.wait(.9)
     end
 end)
 
-_G.AutoUpgrade = not _G.AutoUpgrade
-local Upgrade = tycoon.Purchases["Lemon Stand"]["Lemon Stand"]["Lemon Stand"].Upgrade
+_G.AutoUpgradeLemonStand = not _G.AutoUpgradeLemonStand
+local UpgradeLemonStand = tycoon.Purchases["Lemon Stand"]["Lemon Stand"]["Lemon Stand"].Upgrade
 task.spawn(function()
-    while _G.AutoUpgrade do
-        Upgrade:InvokeServer(1)
+    while _G.AutoUpgradeLemonStand do
+        UpgradeLemonStand:InvokeServer(1)
+        task.wait()
+    end
+end)
+
+_G.AutoClickLemonDash = not _G.AutoClickLemonDash
+local ClickLemonDash = tycoon.Remotes.WakeIncomeStream
+task.spawn(function()
+    while _G.AutoClickLemonDash do
+        ClickLemonDash:InvokeServer("LemonDash")
+        task.wait(15 - .1)
+    end
+end)
+
+_G.AutoUpgradeLemonDash = not _G.AutoUpgradeLemonDash
+local UpgradeLemonDash = tycoon.Purchases.LemonDash.LemonDash.LemonDash.Upgrade
+task.spawn(function()
+    while _G.AutoUpgradeLemonDash do
+        UpgradeLemonDash:InvokeServer(1)
         task.wait()
     end
 end)
